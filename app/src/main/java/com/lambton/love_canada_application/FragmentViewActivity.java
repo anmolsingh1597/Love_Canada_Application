930c97d279e3f2ae258b9c609dca5296af3b6c38
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-07 19:29:43 -0400

    [Anmol Singh]Love_Canada_Application: Multiple Fragments replaced dynamically

--- app/src/main/java/com/lambton/love_canada_application/FragmentViewActivity.java
+++ app/src/main/java/com/lambton/love_canada_application/FragmentViewActivity.java
@@ -1,21 +1,80 @@
 package com.lambton.love_canada_application;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import com.lambton.love_canada_application.fragments.BlueFragment;
+import com.lambton.love_canada_application.fragments.GreenFragment;
+import com.lambton.love_canada_application.fragments.RedFragment;
 
 public class FragmentViewActivity extends AppCompatActivity {
 
     public String toastString;
+    FragmentManager  fragManager;
+    FragmentTransaction fragTransaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_view);
+
+        fragmentActions();
+    }
+
+    private void fragmentActions(){
+
+        fragManager = getSupportFragmentManager();
+        //Add default at first time boot
+        fragTransaction = fragManager.beginTransaction();
+        fragTransaction.add(R.id.container, new RedFragment());
+        fragTransaction.commit();
     }
 
     public void buttonClick(View view){
         Toast.makeText(FragmentViewActivity.this, toastString, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       getMenuInflater().inflate(R.menu.main_menu,menu);
+       return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        fragTransaction = fragManager.beginTransaction();
+        Fragment fragValue = null;
+
+        switch (item.getItemId()){
+            case R.id.redMenuItem:
+                fragValue = new RedFragment();
+                fragTransaction.replace(R.id.container, fragValue);
+                break;
+            case R.id.greenMenuItem:
+                fragValue = new GreenFragment();
+                fragTransaction.replace(R.id.container, fragValue);
+                break;
+            case R.id.blueMenuItem:
+                fragValue = new BlueFragment();
+                fragTransaction.replace(R.id.container, fragValue);
+                break;
+            case R.id.homeMenuItem:
+                fragTransaction.addToBackStack(null);
+                break;
+        }
+
+
+        fragTransaction.commit();
+        return super.onOptionsItemSelected(item);
     }
 }
