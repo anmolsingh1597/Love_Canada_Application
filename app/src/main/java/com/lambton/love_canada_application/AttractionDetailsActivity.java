4b41c11e9683af583f610577f116eb496a5ce39e
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-07 09:16:41 -0400

    [Anmol Singh]Love_Canada_Application: Values Displayed

--- app/src/main/java/com/lambton/love_canada_application/AttractionDetailsActivity.java
+++ app/src/main/java/com/lambton/love_canada_application/AttractionDetailsActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.lambton.love_canada_application.models.CanadaAttraction;
@@ -12,6 +14,13 @@ public class AttractionDetailsActivity extends AppCompatActivity {
 
     Bundle fetchedUser;
     CanadaAttraction object;
+    private ImageView attractionImage;
+    private TextView id;;
+    private TextView name;
+    private TextView address;
+    private TextView city;
+    private TextView province;
+    private TextView description;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +28,25 @@ public class AttractionDetailsActivity extends AppCompatActivity {
         intials();
     }
     public void intials(){
+        getSupportActionBar().hide();
         Intent intent = getIntent();
         fetchedUser = intent.getBundleExtra("object");
         object = (CanadaAttraction) fetchedUser.getSerializable("detailsObject");
-        Toast.makeText(AttractionDetailsActivity.this, object.getPlaceName(), Toast.LENGTH_SHORT).show();
+        attractionImage = findViewById(R.id.detailsImageView);
+        id = findViewById(R.id.idDetailsText);
+        name = findViewById(R.id.nameDetailsText);
+        address = findViewById(R.id.addressDetailsText);
+        city = findViewById(R.id.cityDetailsText);
+        province = findViewById(R.id.provinceDetailsText);
+        description = findViewById(R.id.descriptionDetailsText);
+        attractionImage.setImageResource(object.getImage());
+        id.setText("Id: " + String.valueOf(object.getPlaceId()));
+        name.setText("Name: " + object.getPlaceName());
+        address.setText("Address: " + object.getAddress());
+        city.setText("City: " + object.getCity());
+        province.setText("Province: " + object.getProvince());
+        description.setText("Description: " + object.getDescription());
+//        Toast.makeText(AttractionDetailsActivity.this, object.getPlaceName(), Toast.LENGTH_SHORT).show();
+
     }
 }
